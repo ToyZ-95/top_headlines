bce42ffe2de330dd020feb0e20c15e5f92d7e29e
Asad Patel <patelasad4@gmail.com>
2021-05-19 13:01:34 +0530

    custom card added

--- lib/component/news_card.dart
+++ lib/component/news_card.dart
@@ -1,1 +1,90 @@
+import 'package:flutter/material.dart';
+import 'package:news_app/models/NewsModel.dart';
 
+class NewsCard extends StatefulWidget {
+  final Article article;
+
+  const NewsCard(this.article);
+
+  @override
+  _NewsCardState createState() => _NewsCardState();
+}
+
+class _NewsCardState extends State<NewsCard> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+        height: MediaQuery.of(context).size.height - 300,
+        width: MediaQuery.of(context).size.width - 60,
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(10.0),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Container(
+              height: 200.0,
+              child: ClipRRect(
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(10.0),
+                  topRight: Radius.circular(10.0),
+                ),
+                child: Image(
+                  image: NetworkImage(widget.article.urlToImage),
+                  fit: BoxFit.fill,
+                ),
+              ),
+            ),
+            SizedBox(height: 20.0),
+            Container(
+              child: Flexible(
+                child: SingleChildScrollView(
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(
+                      widget.article.title,
+                      style: TextStyle(
+                        fontSize: 25.0,
+                        fontWeight: FontWeight.w600,
+                        shadows: [
+                          Shadow(
+                            color: Colors.white,
+                            offset: Offset(-1, -1),
+                          ),
+                          Shadow(
+                            color: Colors.white,
+                            offset: Offset(1, -1),
+                          ),
+                          Shadow(
+                            color: Colors.white,
+                            offset: Offset(-1, 1),
+                          ),
+                          Shadow(
+                            color: Colors.white,
+                            offset: Offset(1, 1),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 50.0,
+            ),
+            Container(
+              alignment: Alignment.bottomCenter,
+              child: ElevatedButton(
+                onPressed: () {},
+                child: Text('Read Mode'),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
