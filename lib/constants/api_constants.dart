12f344bb90f284265241364e4fb5604aabc47fdc
Asad <62109756+ToyZ-95@users.noreply.github.com>
2021-05-21 11:17:18 +0530

    Delete api_constants.dart

--- lib/constants/api_constants.dart
+++
@@ -1,6 +0,0 @@
-class APIConstants {
-  static const String newsAPIKey = '31f88efd99c94025bbd5cae58237d062';
-
-  static const String url =
-      'https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=$newsAPIKey';
-}
