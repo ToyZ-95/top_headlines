b601525400190ed9d9c3a4775b32b0bbcfa7430b
Asad <patelasad4@gmail.com>
2021-05-24 22:41:04 +0530

    changes

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -149,7 +149,6 @@ class _HomePageState extends State<HomePage> {
                                             Countries.selectedCountry];
                                   }
                                   Navigator.pop(context);
-                                  Navigator.pop(context);
                                   Navigator.push(
                                     context,
                                     MaterialPageRoute(
